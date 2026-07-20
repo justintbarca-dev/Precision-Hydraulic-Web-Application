d02525200d83e139cfc3f3c45e97c9e9802656f6
justintbarca-dev <justintbarca@gmail.com>
2026-07-20 18:11:36 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'precision-hydraulic-labor-layout-v15-sealqty-v2';
+const CACHE_NAME = 'precision-hydraulic-labor-panel-seal-horizontal-v16';
 const BASE = 'https://justintbarca-dev.github.io/Precision-Hydraulic-Web-Application/';
 const APP_SHELL = [
   BASE,
