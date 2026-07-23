c95a9f67ff98a9cc2c208e51bac9fe2b84fe45b7
justintbarca-dev <justintbarca@gmail.com>
2026-07-23 12:55:05 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'precision-hydraulic-returned-v1';
+const CACHE_NAME = 'precision-hydraulic-company-jobs-rod-case-closefix-v1';
 const BASE = 'https://justintbarca-dev.github.io/Precision-Hydraulic-Web-Application/';
 const APP_SHELL = [
   BASE,
